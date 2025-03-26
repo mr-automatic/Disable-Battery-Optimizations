d50ff9db0cecb6619ebcdf87722643f15951da4e
pchkauu <pchkauu@gmail.com>
2025-03-26 03:12:09 +0300

    chore: upgrade Android SDK versions and modernize preferences
    - Update compileSdkVersion and targetSdkVersion to 35
    
    - Increase minSdkVersion to 23
    
    - Add compiler arguments for deprecation warnings
    
    - Modernize SharedPreferences implementation:
    
      * Replace deprecated PreferenceManager with Context-based preferences
    
      * Add dedicated PREF_NAME constant
    
      * Improve code organization and documentation

--- android/src/main/java/in/jvapps/disable_battery_optimization/utils/PrefUtils.java
+++ android/src/main/java/in/jvapps/disable_battery_optimization/utils/PrefUtils.java
@@ -2,104 +2,96 @@ package in.jvapps.disable_battery_optimization.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.util.Log;
 
-import java.lang.ref.WeakReference;
-import java.util.Objects;
+import androidx.annotation.NonNull;
 
 public class PrefUtils {
 
-    /**
-     * Called to save supplied value in shared preferences against given key.
-     *
-     * @param context Context of caller activity
-     * @param key     Key of value to save against
-     * @param value   Value to save
-     */
-    public static void saveToPrefs(Context context, String key, Object value) {
-        WeakReference<Context> contextWeakReference = new WeakReference<>(context);
-        if (contextWeakReference.get() != null) {
-            SharedPreferences prefs =
-                    PreferenceManager.getDefaultSharedPreferences(contextWeakReference.get());
-            final SharedPreferences.Editor editor = prefs.edit();
-            if (value instanceof Integer) {
-                editor.putInt(key, (Integer) value);
-            } else if (value instanceof String) {
-                editor.putString(key, value.toString());
-            } else if (value instanceof Boolean) {
-                editor.putBoolean(key, (Boolean) value);
-            } else if (value instanceof Long) {
-                editor.putLong(key, (Long) value);
-            } else if (value instanceof Float) {
-                editor.putFloat(key, (Float) value);
-            } else if (value instanceof Double) {
-                editor.putLong(key, Double.doubleToRawLongBits((double) value));
-            }
-            editor.apply();
-        }
+    private static final String PREF_NAME = "disable_battery_optimization_prefs";
+    private static final String TAG = "PrefUtils";
+
+    private static SharedPreferences getPrefs(@NonNull Context context) {
+        return context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
     }
 
     /**
-     * Called to retrieve required value from shared preferences, identified by given key.
-     * Default value will be returned of no value found or error occurred.
-     *
-     * @param context      Context of caller activity
-     * @param key          Key to find value against
-     * @param defaultValue Value to return if no data found against given key
-     * @return Return the value found against given key, default if not found or any error occurs
+     * Сохраняет значение в SharedPreferences.
      */
-    public static Object getFromPrefs(Context context, String key, Object defaultValue) {
-        WeakReference<Context> contextWeakReference = new WeakReference<>(context);
-        if (contextWeakReference.get() != null) {
-            SharedPreferences sharedPrefs =
-                    PreferenceManager.getDefaultSharedPreferences(contextWeakReference.get());
-            try {
-                if (defaultValue instanceof String) {
-                    return sharedPrefs.getString(key, defaultValue.toString());
-                } else if (defaultValue instanceof Integer) {
-                    return sharedPrefs.getInt(key, (Integer) defaultValue);
-                } else if (defaultValue instanceof Boolean) {
-                    return sharedPrefs.getBoolean(key, (Boolean) defaultValue);
-                } else if (defaultValue instanceof Long) {
-                    return sharedPrefs.getLong(key, (Long) defaultValue);
-                } else if (defaultValue instanceof Float) {
-                    return sharedPrefs.getFloat(key, (Float) defaultValue);
-                } else if (defaultValue instanceof Double) {
-                    return Double.longBitsToDouble(sharedPrefs.getLong(key, Double.doubleToLongBits((double) defaultValue)));
-                }
-            } catch (Exception e) {
-                Log.e("Exception", Objects.requireNonNull(e.getMessage()));
+    public static void saveToPrefs(@NonNull Context context, @NonNull String key, @NonNull Object value) {
+        SharedPreferences prefs = getPrefs(context);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        if (value instanceof Integer) {
+            editor.putInt(key, (Integer) value);
+        } else if (value instanceof String) {
+            editor.putString(key, (String) value);
+        } else if (value instanceof Boolean) {
+            editor.putBoolean(key, (Boolean) value);
+        } else if (value instanceof Long) {
+            editor.putLong(key, (Long) value);
+        } else if (value instanceof Float) {
+            editor.putFloat(key, (Float) value);
+        } else if (value instanceof Double) {
+            // SharedPreferences не поддерживает Double напрямую
+            editor.putLong(key, Double.doubleToRawLongBits((Double) value));
+        } else {
+            Log.w(TAG, "Unsupported value type: " + value.getClass().getName());
+            return;
+        }
+
+        editor.apply();
+    }
+
+    /**
+     * Получает значение из SharedPreferences.
+     */
+    public static Object getFromPrefs(@NonNull Context context, @NonNull String key, @NonNull Object defaultValue) {
+        SharedPreferences prefs = getPrefs(context);
+        try {
+            if (defaultValue instanceof String) {
+                return prefs.getString(key, (String) defaultValue);
+            } else if (defaultValue instanceof Integer) {
+                return prefs.getInt(key, (Integer) defaultValue);
+            } else if (defaultValue instanceof Boolean) {
+                return prefs.getBoolean(key, (Boolean) defaultValue);
+            } else if (defaultValue instanceof Long) {
+                return prefs.getLong(key, (Long) defaultValue);
+            } else if (defaultValue instanceof Float) {
+                return prefs.getFloat(key, (Float) defaultValue);
+            } else if (defaultValue instanceof Double) {
+                return Double.longBitsToDouble(prefs.getLong(key, Double.doubleToLongBits((Double) defaultValue)));
+            } else {
+                Log.w(TAG, "Unsupported default value type: " + defaultValue.getClass().getName());
                 return defaultValue;
             }
+        } catch (Exception e) {
+            Log.e(TAG, "Error getting value from prefs", e);
+            return defaultValue;
         }
-        return defaultValue;
     }
 
     /**
-     * @param context Context of caller activity
-     * @param key     Key to delete from SharedPreferences
+     * Удаляет значение по ключу.
      */
-    public static void removeFromPrefs(Context context, String key) {
-        WeakReference<Context> contextWeakReference = new WeakReference<>(context);
-        if (contextWeakReference.get() != null) {
-            SharedPreferences prefs =
-                    PreferenceManager.getDefaultSharedPreferences(contextWeakReference.get());
-            final SharedPreferences.Editor editor = prefs.edit();
-            editor.remove(key);
-            editor.apply();
-        }
+    public static void removeFromPrefs(@NonNull Context context, @NonNull String key) {
+        SharedPreferences prefs = getPrefs(context);
+        prefs.edit().remove(key).apply();
     }
 
-    @SuppressWarnings("unused")
-    public static boolean hasKey(Context context, String key) {
-        WeakReference<Context> contextWeakReference = new WeakReference<>(context);
-        if (contextWeakReference.get() != null) {
-            SharedPreferences prefs =
-                    PreferenceManager.getDefaultSharedPreferences(contextWeakReference.get());
-            return prefs.contains(key);
-        }
-        return false;
+    /**
+     * Проверяет наличие ключа.
+     */
+    public static boolean hasKey(@NonNull Context context, @NonNull String key) {
+        SharedPreferences prefs = getPrefs(context);
+        return prefs.contains(key);
     }
 
+    /**
+     * Очищает все данные.
+     */
+    public static void clearAll(@NonNull Context context) {
+        SharedPreferences prefs = getPrefs(context);
+        prefs.edit().clear().apply();
+    }
 }
